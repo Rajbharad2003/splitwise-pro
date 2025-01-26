de424e1606ed2b5d26fadb446b69f63fb6000f4f
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2025-01-26 10:41:11 +0530

    sign up/in page

--- splitwise-pro/client/app/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-import { useEffect } from "react";
-import { StyleSheet } from "react-native";
-import '../global.css';
-
-export default function RootLayout() {
-  return <Stack />;
-}
--- splitwise-pro/client/babel.config.js
+++ splitwise-pro/client/babel.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+module.exports = function (api) {
+  api.cache(true);
+  return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: ["nativewind/babel"],
+
   };
-  
+};
